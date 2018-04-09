f22db7f2314c9e186fd144d1604921cdd4bfeee7
Demarteau, Martijn (1621343) <1621343demarteau@zuyd.nl>
2018-04-09 18:18:29 +0000

    Merged PR 4: Loginpagina en paar data chagnes

--- Data.Entities/Contract.cs
+++ Data.Entities/Contract.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Data.Entities
 {
-    public class Class1
+    public class Contract
     {
     }
 }
---
+++ Data.Entities/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public string EMail { get; set; }
+        public string PhoneNumber { get; set; }
+        public Address AddressId { get; set; }
+        public Contract ContractId { get; set; }
+        public MeterReading MeterReadingId { get; set; }
+        public Report ReportId { get; set; }
+        public Password PasswordId { get; set; }
+        public Bill BillId { get; set; }
+        public DateTime Valid_From { get; set; }
+        public DateTime Valid_Through { get; set; }
+        public DateTime Last_Update { get; set; }
+    }
+}
