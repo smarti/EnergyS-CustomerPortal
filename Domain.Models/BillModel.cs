352b7032d933136d9bb16922326585a5465c7298
Marco Goessens <1622935goessens@zuyd.nl>
2018-04-11 12:34:06 +0200

    Billmodel

--- Domain.Models/BillModel.cs
+++ Domain.Models/BillModel.cs
@@ -8,5 +8,9 @@ namespace Domain.Models
 {
     public class BillModel
     {
+        public int BillId { get; set; }
+        public int Amount { get; set; }
+        public string PaymentStatus { get; set; }
+        public DateTime LastUpdate { get; set; }
     }
 }
