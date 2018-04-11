77cb94ccda7b13cea40b2eb5e37f2c65abb39b80
Smarti MD <martijneau@live.nl>
2018-04-11 12:53:02 +0200

    Changed Report Entity, Added Report Data Class, Added ReportModel

--- Domain.Models/ReportModel.cs
+++ Domain.Models/ReportModel.cs
@@ -8,5 +8,17 @@ namespace Domain.Models
 {
     public class ReportModel
     {
+        public ReportModel(int reportId, string description, string descriptionStatus, DateTime lastUpdate)
+        {
+            ReportId = reportId;
+            Description = description;
+            DescriptionStatus = descriptionStatus;
+            LastUpdate = lastUpdate;
+        }
+
+        public int ReportId { get; set; }
+        public string Description { get; set; }
+        public string DescriptionStatus { get; set; }
+        public DateTime LastUpdate { get; set; }
     }
 }
