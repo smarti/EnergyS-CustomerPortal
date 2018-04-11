638341ea059d4b7e8f2950ebc7adbba39e187568
Smarti MD <martijneau@live.nl>
2018-04-11 14:39:43 +0200

    Added conversion for password and address in Domain layer

--- Domain.Models/Map.cs
+++ Domain.Models/Map.cs
@@ -1,26 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Data.Entities;
 
 namespace Domain.Models
 {
     public static class Map
     {
-
         public static Customer CustomerModelToCustomer(CustomerModel customerModel)
         {
-            Customer convertedCustomer = new Customer
+            var convertedCustomer = new Customer
             {
                 CustomerId = customerModel.CustomerId,
                 FirstName = customerModel.FirstName,
                 LastName = customerModel.LastName,
                 EMail = customerModel.EMail,
                 PhoneNumber = customerModel.PhoneNumber,
-                //Address = customerModel.Address.MapToAddress(),
-                //Password = customerModel.Password.MapToPassword()
+                Address = AddressModelToAddress(customerModel.Address),
+                Password = PasswordModelToPassword(customerModel.Password)
             };
 
             return convertedCustomer;
@@ -28,20 +23,38 @@ namespace Domain.Models
 
         public static CustomerModel CustomerToCustomerModel(Customer customer)
         {
-            CustomerModel convertedCustomer = new CustomerModel
+            var convertedCustomer = new CustomerModel
             {
                 CustomerId = customer.CustomerId,
                 FirstName = customer.FirstName,
                 LastName = customer.LastName,
                 EMail = customer.EMail,
                 PhoneNumber = customer.PhoneNumber,
-                //Address = customer.Address.MapToAddress(),
-                //Password = customer.Password.MapToPassword()
+                Address = AddressToAddressModel(customer.Address),
+                Password = PasswordToPasswordModel(customer.Password)
             };
 
             return convertedCustomer;
         }
 
+        private static Password PasswordModelToPassword(PasswordModel customerModelPassword)
+        {
+            throw new NotImplementedException();
+        }
 
+        private static PasswordModel PasswordToPasswordModel(Password customerPassword)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Address AddressModelToAddress(AddressModel customerModelAddress)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static AddressModel AddressToAddressModel(Address customerAddress)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
