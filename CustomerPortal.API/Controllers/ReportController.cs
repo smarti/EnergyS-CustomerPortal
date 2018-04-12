3454509b9a9d3153d84c82c17a20c3ebcb0d9445
Smarti MD <martijneau@live.nl>
2018-04-12 22:10:14 +0200

    Added API controller

---
+++ CustomerPortal.API/Controllers/ReportController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Application.Providers;
+using Application.ViewModels;
+
+namespace CustomerPortal.API.Controllers
+{
+    [Route("api/{customerId}/reports")]
+    public class ReportController : ApiController
+    {
+        private ReportProvider _provider;
+
+        public ReportController() : base()
+        {
+            _provider = new ReportProvider();
+        }
+
+        public List<ReportViewModel> GetAllReportsByCustomer(int customerId)
+        {
+            CustomerViewModel customer = new CustomerViewModel { CustomerId = customerId };
+
+            List<ReportViewModel> reports = _provider.GetAllReportsByCustomer(customer);
+
+            return reports;
+        }
+    }
+}
