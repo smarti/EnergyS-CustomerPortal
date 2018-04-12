9c423f286585c0315ce3471c8841e626c55dc0ea
Smarti MD <martijneau@live.nl>
2018-04-12 15:16:05 +0200

    Added application layer

---
+++ Application.ViewModels/ReportViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.ViewModels
+{
+    class ReportViewModel
+    {
+        public int ReportId { get; set; }
+        public string Description { get; set; }
+        public string DescriptionStatus { get; set; }
+        public DateTime LastUpdate { get; set; }
+    }
+}
