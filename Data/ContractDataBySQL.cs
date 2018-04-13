c779b7387f4a84609a9aa75391d363826d5d10fa
Marco Goessens <1622935goessens@zuyd.nl>
2018-04-13 12:56:27 +0200

    Contract data by SQL

---
+++ Data/ContractDataBySQL.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Entities;
+
+namespace Data
+{
+    public class ContractDataBySQL
+    {
+        private readonly DBContext Context;
+
+        public ContractDataBySQL(DBContext context)
+        {
+            Context = context;
+        }
+        public List<Contract> GetAllBillsByCustumer(Customer customer)
+        {
+            List<Contract> contracts = new List<Contract>();
+
+            contracts.AddRange(Context.Contracts.Where(contract => contract.Customer.CustomerId == customer.CustomerId));
+
+            return contracts;
+        }
+    }
+}
