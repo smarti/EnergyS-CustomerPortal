2df7356d05b3ace8ae5328ea292ff04adc56e089
Smarti MD <martijneau@live.nl>
2018-04-20 10:07:22 +0200

    Ran code cleanup on domain models

--- Domain.Models/MeterReadingModel.cs
+++ Domain.Models/MeterReadingModel.cs
@@ -1,21 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Domain.Models
 {
     public class MeterReadingModel
     {
-        
         public int MeterReadingId { get; set; }
 
         public int CurrentReading { get; set; }
         public DateTime LastUpdate { get; set; }
-
-
-
-    
-}
+    }
 }
